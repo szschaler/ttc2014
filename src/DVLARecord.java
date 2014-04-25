b17270d67ff8d01a85cefe412b0cbe83c989dd48
sobhz <k0941360@kcl.ac.uk>
2014-04-25 18:45:52 +0100

    Regarding c# template In the BuildCS.xml =><superimpose artifactHandler="java15" /> has to be modified according to c# In the BuildCS.xml=><typedef name="superimpose" classname="org.eclipse.epsilon.egl.symmetric_ao.tasks.superimpose.SuperimpositionMerge"/>
    Current Error:
    BUILD FAILED
    C:\Users\localadmin\ttc2014\TTC2014\workflow\buildCS.xml:82: Problem: failed to create task or type epsilon.xml.loadModel
    Cause: The name is undefined.
    Action: Check the spelling.
    Action: Check that any custom tasks/types have been declared.
    Action: Check that any <presetdef>/<macrodef> declarations have taken place.

---
+++ src/DVLARecord.java
@@ -0,0 +1,16 @@
+public  class  DVLARecord {
+	
+  private String status0 =   "OffRoad" ;
+
+	
+
+  public DVLARecord() {}
+
+	
+  
+  public DVLARecord(String status0) {
+   			 this.status0 = status0;
+  }
+
+
+}
