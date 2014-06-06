52eecdea1bf73b74259b298cb39cbcd49c18a291
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2014-06-06 13:04:18 +0100

    Initial setup for merging fields into collections: Created new field composition rule and registered it in workflow.

--- src/uk/ac/kcl/inf/smal/ttc2014/ant/compositionrules/AdvancedFieldComposition.java
+++ src/uk/ac/kcl/inf/smal/ttc2014/ant/compositionrules/AdvancedFieldComposition.java
@@ -1,5 +1,8 @@
 package uk.ac.kcl.inf.smal.ttc2014.ant.compositionrules;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.eclipse.epsilon.egl.symmetric_ao.tasks.superimpose.rules.FieldOverrideRule;
 
 import de.ovgu.cide.fstgen.ast.FSTNonTerminal;
@@ -14,18 +17,86 @@ import de.ovgu.cide.fstgen.ast.FSTTerminal;
  * 
  */
 public class AdvancedFieldComposition extends FieldOverrideRule {
-	
-	@Override
-	public boolean areEqual(FSTTerminal terminalA, FSTTerminal terminalB) {
-		System.out.println("areEqual called.");
-		return false;
+
+	/**
+	 * Pattern for matching the modifiers, (first) name, and type of a field
+	 * declaration.
+	 */
+	private static Pattern pModifiers = Pattern.compile("^[^,;=]+");
+
+	/**
+	 * Extract the field type from a given field declaration terminal.
+	 * 
+	 * @param t
+	 * @return
+	 */
+	private String getType(FSTTerminal t) {
+		String sBody = t.getBody();
+
+		Matcher m = pModifiers.matcher(sBody);
+		if (m.matches()) {
+			String[] saModifiers = m.group().split("\\s+");
+
+			// The last but one word should be the type of the field, assuming
+			// we're dealing with syntactically correct code here
+			return saModifiers[saModifiers.length - 2];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Set the type of the given field declaration. Also removes assignments, if
+	 * any.
+	 * 
+	 * @param t
+	 * @param newType
+	 */
+	private void setType(FSTTerminal t, String newType) {
+		String sBody = t.getBody();
+
+		Matcher m = pModifiers.matcher(sBody);
+		if (m.matches()) {
+			String[] saModifiers = m.group().split("\\s+");
+
+			// The last but one word should be the type of the field, assuming
+			// we're dealing with syntactically correct code here
+			saModifiers[saModifiers.length - 2] = newType;
+
+			// TODO: Switch to Java8 and use set stream API
+			String sRecombined = "";
+			for (String sModifier : saModifiers) {
+				sRecombined += sModifier;
+			}
+
+			StringBuffer sbResult = new StringBuffer();
+			m.appendReplacement(sbResult, sRecombined).appendTail(sbResult);
+
+			// Strip out any assignments and set body of field declaration
+			t.setBody(sbResult.toString().replaceAll("=[^;]+;", ";"));
+		}
 	}
 
 	@Override
 	public void compose(FSTTerminal terminalA, FSTTerminal terminalB,
 			FSTTerminal terminalComp, FSTNonTerminal nonterminalParent) {
-		System.out.println("Composing two fields: " + terminalA.getBody() + " << >> " + terminalB.getBody());
-		
+
+		String typeA = getType(terminalA);
+		String typeB = getType(terminalB);
+
+		if (!typeA.equals(typeB)) {
+			// Merge types if possible
+			if (typeA.equals("Set<" + typeB + ">")) {
+				setType(terminalB, typeA);
+			} else if (typeB.equals("Set<" + typeA + ">")) {
+				setType(terminalA, typeB);
+			} else {
+				throw new UnsupportedOperationException(
+						"Cannot currently unify these types: " + typeA + ", "
+								+ typeB);
+			}
+		}
+
 		super.compose(terminalA, terminalB, terminalComp, nonterminalParent);
 	}
 
